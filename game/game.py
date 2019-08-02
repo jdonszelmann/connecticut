68381f5a8df88098c5c8c38d571057d74a873a8a
Jonathan Donszelmann <jonabent@gmail.com>
2019-08-02 16:15:41 +0200

    added multiple games per user

--- game/game.py
+++ game/game.py
@@ -1,20 +1,22 @@
 from . import database
 from .player import Player
 from .config import Config
-from peewee import ForeignKeyField, IntegerField
+from peewee import ForeignKeyField, IntegerField, CharField
 
 
 class Game(database.BaseModel):
-    player1 = ForeignKeyField(Player, backref="game", null=True)
-    player2 = ForeignKeyField(Player, backref="game", null=True)
+    player1 = ForeignKeyField(Player, backref="games1", null=True)
+    player2 = ForeignKeyField(Player, backref="games2", null=True)
     width = IntegerField()
     height = IntegerField()
+    name = CharField()
 
     @classmethod
     def create_default(cls, *args, **kwargs):
 
         width = int(Config.get_config_option("defaultwidth"))
         height = int(Config.get_config_option("defaultheight"))
+        name = Config.get_config_option("defaultname")
 
         return cls.create(*args, width=width, height=height, **kwargs)
 
